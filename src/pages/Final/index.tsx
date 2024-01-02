3abf39fc1d915b9e6d6a2ffb1f0d59858c42a55c
최성훈 <chltjdgns1009@gmail.com>
2024-01-02 10:39:50 +0900

    download

--- src/pages/Final/index.tsx
+++ src/pages/Final/index.tsx
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import * as S from './style'
 import { useAtom } from 'jotai';
 import { atomColor, photo } from '../../atoms/photo';
 import selectBackgroundColor from 'helper/selectBackgroundColor';
+import { toPng } from 'html-to-image';
 
 const Final = () => {
   const [finalImg,getFinalImg] = useAtom(photo)
   const [selectedColorIndex, setSelectedColorIndex] = useAtom(atomColor);
 
+  const imgref = useRef(null);
+
+  const onDownloadBtn = useCallback(() => {
+    if (imgref.current === null) {
+      return;
+    }
+
+    toPng(imgref.current)
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = "my-image-name.png";
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [imgref]);
+
   return (
     <S.Layout>
-      <S.FinalLayout backgroundColor={selectBackgroundColor(selectedColorIndex)} >
+      <S.FinalLayout backgroundColor={selectBackgroundColor(selectedColorIndex)} ref={imgref} >
         <S.FinalImgs>
           {
             finalImg.map((item,index) => (
@@ -19,6 +39,7 @@ const Final = () => {
           }
         </S.FinalImgs>
       </S.FinalLayout>
+      <button onClick={onDownloadBtn} >다운로드</button>
     </S.Layout>
   );
 };
